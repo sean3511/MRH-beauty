d10a3aa294b14ad26a803a35290c8be1e5f1c1e5
Sloly <yahoo3511@gmail.com>
2022-10-25 04:25:22 +0800

    新增行數限制，微調版面

--- java.js
+++ java.js
@@ -1,3 +1,14 @@
+// ham切換
 $(".ham").click(function(){
     $(".header-globalnavi").fadeToggle(300);
 });
+// 字體數量限制
+// $(".mainvisual-news-title").each(function(){
+//     var len=$(this).text().length;   //當前HTML物件text的長度
+//     console.log(len);
+//     if(len>16){
+//         var str="";
+//         str=$(this).text().substring(0,16)+"...";  //使用字串擷取，獲取前30個字元，多餘的字元使用“......”代替
+//         $(this).html(str);                   //將替換的值賦值給當前物件
+//     }
+// });
